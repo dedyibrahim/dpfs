508bd1d5f2d610f500617a4b72b4638204c5d313
ronee766 <ronialfiansyah6690@gmail.com>
2017-12-22 01:45:21 +0700

    PUSH RONI

--- application/views/V_anamnesa/V_form_anamnesa.php
+++ application/views/V_anamnesa/V_form_anamnesa.php
@@ -1,4 +1,136 @@
+<div class="x_panel">
 
+<div class="col-md-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Form Basic Elements <small>different form elements</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left">
 
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Kegiatan</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="text" class="form-control" placeholder="Kegiatan">
+                        </div>
+                      </div>
+                      
+                      
+                      <div class="form-group">
+                          <label class="control-label col-md-3 col-sm-3 col-xs-12">Tanggal Pengambilan Sampel</label>
+      <div class="input-group date" id="myDatepicker">
+          <input class="form-control" name="tgl_penerimaan" type="text" placeholder="Tanggal penerimaan">
+                            <span class="input-group-addon" style="">
+                               <span class="glyphicon glyphicon-calendar"></span>
+                            </span>
+      </div>
+                      </div>
+                        <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Nama Pemilik</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="text" class="form-control" placeholder="Nama Pemilik">
+                        </div>
+                      </div>
+                        
+                        <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Lokasi Sampling</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="text" class="form-control" placeholder="Lokasi Sampling">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                      <div class="radio">
+                          
+                          <label class="control-label col-md-3 col-sm-3 col-xs-12">Asal Sample</label>
+                            <label>
+                              <input type="radio" checked="" value="option1" id="optionsRadios1" name="optionsRadios" class> Budaya
+                            </label>
+                          </div>
+                          <div class="radio">
+                            <label>
+                              <input type="radio" value="option2" id="optionsRadios2" name="optionsRadios"> Hasil Tangkapan
+                            </label>
+                          </div>
+                        </div>
+                         <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Jenis Sample</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="text" class="form-control" placeholder="Jenis Sample">
+                        </div>
+                      </div>
+                        
+                         <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Jumlah Sample</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="text" class="form-control" placeholder="Jumlah Sample">
+                        </div>
+                      </div>
+     <div class="form-group">
+                      <div class="radio">
+                          
+                          <label class="control-label col-md-3 col-sm-3 col-xs-12">Bentuk Sample</label>
+                            <label>
+                              <input type="radio" checked="" value="option1" id="optionsRadios1" name="optionsRadios1" class> Segar
+                            </label>
+                          </div>
+                          <div class="radio">
+                            <label>
+                              <input type="radio" value="option2" id="optionsRadios1" name="optionsRadios1"> Hidup
+                            </label>
+                          </div>
+                            <div class="radio">
+                            <label>
+                              <input type="radio" value="option2" id="optionsRadios1" name="optionsRadios1"> Beku
+                            </label>
+                          </div>
+                          <div class="radio">
+                            <label>
+                              <input type="radio" value="option2" id="optionsRadios1" name="optionsRadios1"> Kering
+                            </label>
+                          </div>
+                          <div class="radio">
+                            <label>
+                              <input type="radio" value="option2" id="optionsRadios1" name="optionsRadios1"> Lainnya
+                            </label>
+                          </div>
+                        </div>
+      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Kode Sample</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="text" class="form-control" placeholder="Kode Sample">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Gejala Klinis</label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="text" class="form-control" placeholder="Gejala Klinis">
+                        </div>
+                        <div class="ln_solid"></div>
 
-</div>
+                  <div class="form-group">
+                    <label class="control-label col-md-3 col-sm-3 col-xs-12">Keterangan Lain-Lain</label>
+                    <div class="col-md-9 col-sm-9 col-xs-12">
+                      <textarea class="resizable_textarea form-control" placeholder="Keterangan Lain"></textarea>
+                    </div>
+                  </div>
+                </div>
+                      </div>
+                      
+                      </div>
