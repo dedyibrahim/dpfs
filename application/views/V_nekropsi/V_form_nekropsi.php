2ce8c95ab0bcaa7e964d3e18ad47c804f35cd42a
ronee766 <ronialfiansyah6690@gmail.com>
2017-12-25 12:58:08 +0700

    v form nekrop

--- application/views/V_nekropsi/V_form_nekropsi.php
+++ application/views/V_nekropsi/V_form_nekropsi.php
@@ -3,7 +3,7 @@
     <div class="x_title">
   
  <form action="<?php echo base_url('C_anamnesa')?>" method="post" enctype="multipart/formdata">
-    <h2>Informasi Kegiatan Anamnesa</h2>
+    <h2>Buku Nekropsi</h2>
         <ul class="nav navbar-right panel_toolbox">
            <li><input class="form-control"  readonly="" name="record_number" placeholder="record number" type="text">
                 </li>
@@ -13,51 +13,48 @@
 <div class="col-md-6">
        
      <div class="form-group">
-         <label>Kegiatan</label>
+         <label>No</label>
          <input  readonly="" type="text" value ="" class="form-control" placeholder="Kegiatan">
      </div>
        
       <div class="input-group date" id="datetimepicker6">
-          <label>tanggal sampling</label>
-          <input readonly="" class="form-control" type="text" name="tgl_sampling" placeholder="Tanggal sampling">
+          <label>tanggal kegiatan</label>
+          <input readonly="" class="form-control" type="text" name="tgl_sampling" placeholder="Tanggal Kegiatan">
                 <span class="input-group-addon">
                     <span class="glyphicon glyphicon-calendar"></span>
                 </span>
        </div>
      
      <div class="form-group">
-         <label>Nama pemilik</label>
-         <input readonly="" type="text" class="form-control" placeholder="Nama Pemilik">
+         <label>Kode Sample</label>
+         <input readonly="" type="text" class="form-control" placeholder="Kode Sample">
      </div>
      
     <div class="form-group">
-        <label>Lokasi sampling</label>
-         <input readonly="" type="text" class="form-control" placeholder="Lokasi Sampling">
+        <label>UKURAN</label>
+         <input type="text" class="form-control" placeholder="Panjang">
+         <input type="text" class="form-control" placeholder="Berat">
        </div>
-
     <div class="form-group">
-        <label>Asal sample</label>
-         <select  readonly="" class="form-control">
-                            <option>--Asal sample--</option>
-                            <option>Budidaya</option>
-                            <option>Hasil tangkapan</option>
-                </select>
-       </div>    
-             
-     
-       <div class="form-group">
-           <label>Jenis sample</label>
-               
-         <input readonly="" type="text" value ="" class="form-control" placeholder="Jenis Sample">
+         <label>Gejala Klinis</label>
+         <input readonly="" type="text" class="form-control" placeholder="Gejala Klinis">
+     </div>
+    
+    <div class="col-md-6">
+<div class="form-group">
+        <label>Organ Target</label>
+         <input type="text" class="form-control" placeholder="Parasit">
+         <input type="text" class="form-control" placeholder="Bakteri">
+         <input type="text" class="form-control" placeholder="Jamur">
+         <input type="text" class="form-control" placeholder="Virus">
        </div>
-       
-       <div class="form-group">
-           <label>Jumlah sample</label>
-         <input readonly="" type="text" value ="" class="form-control" placeholder="Jumlah Sample">
+    
+    <div class="form-group">
+        <label>Analis</label>
+         <input type="text" class="form-control" placeholder="Analis">
        </div>
-</div>
-         
-   </div>
+    
+    
 </form>
- 
+</div>
 </div>
